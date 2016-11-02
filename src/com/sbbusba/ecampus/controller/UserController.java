ae7f99190de9a45c27442b60cc9a2455a3fda48a
Sagheer Rajper <sagheer_rajper@hotmail.com>
2016-11-02 09:57:28 +0500

    today work

--- src/com/sbbusba/ecampus/controller/UserController.java
+++ src/com/sbbusba/ecampus/controller/UserController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.sbbusba.ecampus.model.User;
-import com.sbbusba.ecampus.service.UserService;
 import com.sbbusba.ecampus.service.UserServiceInterface;
 
 @Controller
